e9aefc5be7e5aaa91d8319e87e9bc408e317124f
Raihan Nishat <raihan.nishat@streamstech.com>
2024-08-07 12:47:06 +0600

    modify category entity

--- MongoDBProvider/EFCoreForMongoDB/Entities/Category.cs
+++ MongoDBProvider/EFCoreForMongoDB/Entities/Category.cs
@@ -5,5 +5,4 @@ public class Category : IEntity
     public string Id { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public ICollection<Product> Products { get; set; } = [];
 }
